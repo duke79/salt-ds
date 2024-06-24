680cafb37a2fafcdc7d3a4bbf0868396cfc44c7b
Tom Hazledine <tom@tomhazledine.com>
2024-06-24 14:16:58 +0100

    Adding sub-step styles to TrackerStep

--- packages/lab/src/stepped-tracker/TrackerStep/TrackerStep.tsx
+++ packages/lab/src/stepped-tracker/TrackerStep/TrackerStep.tsx
@@ -3,6 +3,8 @@ import {
   StepActiveIcon,
   StepDefaultIcon,
   StepSuccessIcon,
+  SuccessIcon,
+  ProgressInprogressIcon,
 } from "@salt-ds/icons";
 import { useComponentCssInjection } from "@salt-ds/styles";
 import { useWindow } from "@salt-ds/window";
@@ -19,7 +21,8 @@ import trackerStepCss from "./TrackerStep.css";
 
 const withBaseName = makePrefixer("saltTrackerStep");
 
-type State = "default" | "completed";
+type State = "default" | "completed" | "inprogress";
+type Depth = 0 | 1 | 2;
 
 type StateWithActive = State | "active";
 
@@ -28,24 +31,35 @@ export interface TrackerStepProps extends ComponentPropsWithoutRef<"li"> {
    * The state of the TrackerStep
    */
   state?: State;
+  /**
+   * The nesting depth of the TrackerStep
+   */
+  depth?: Depth;
 }
 
 const iconMap = {
   default: StepDefaultIcon,
   completed: StepSuccessIcon,
+  "completed-sub": SuccessIcon,
+  inprogress: ProgressInprogressIcon,
 };
 
 const getStateIcon = ({
   isActive,
   state,
+  depth = 0,
 }: {
   isActive: boolean;
   state: State;
+  depth: Depth;
 }) => {
   if (state === "default" && isActive) {
     return StepActiveIcon;
   }
-  return iconMap[state];
+  const parsedState =
+    depth > 0 && state === "completed" ? "completed-sub" : state;
+  console.log({ parsedState });
+  return iconMap[parsedState];
 };
 
 const getState = ({
@@ -80,6 +94,7 @@ export const TrackerStep = forwardRef<HTMLLIElement, TrackerStepProps>(
       style,
       className,
       children,
+      depth = 0,
       ...restProps
     } = props;
 
@@ -97,10 +112,11 @@ export const TrackerStep = forwardRef<HTMLLIElement, TrackerStepProps>(
     useCheckWithinSteppedTracker(isWithinSteppedTracker);
 
     const isActive = activeStep === stepNumber;
-    const Icon = getStateIcon({ isActive, state });
+    const Icon = getStateIcon({ isActive, state, depth });
     const resolvedState = getState({ isActive, state });
     const connectorState = activeStep > stepNumber ? "active" : "default";
     const hasConnector = stepNumber < totalSteps - 1;
+    const depthClass = withBaseName(`depth-${depth}`);
 
     const innerStyle = {
       ...style,
@@ -109,14 +125,21 @@ export const TrackerStep = forwardRef<HTMLLIElement, TrackerStepProps>(
 
     return (
       <li
-        className={clsx(withBaseName(), withBaseName(resolvedState), className)}
+        className={clsx(
+          withBaseName(),
+          withBaseName(resolvedState),
+          depthClass,
+          className
+        )}
         style={innerStyle}
         aria-current={isActive ? "step" : undefined}
         data-state={state}
         ref={ref}
         {...restProps}
       >
-        <Icon />
+        <div className={withBaseName("indicator")}>
+          <Icon />
+        </div>
         {hasConnector && <TrackerConnector state={connectorState} />}
         <div className={withBaseName("body")}>{children}</div>
       </li>
